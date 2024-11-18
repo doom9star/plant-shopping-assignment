b7f3f966cc0a43be7cbf0898ee64afb36a07cefc
doom9star <karthikvasudevan18@gmail.com>
2024-11-18 15:51:43 +0530

    modified home!

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -26,13 +26,13 @@ export default function Home() {
             <span className="bg-white text-success px-2">
               one-stop destination for all things green
             </span>{" "}
-            and beautiful! Whether you're a seasoned gardener or just starting
-            your journey of nurturing plants, we offer a wide variety of options
-            to suit every space and style. From vibrant indoor plants that
-            purify the air to low-maintenance succulents perfect for busy
+            and beautiful! Whether you&apos;re a seasoned gardener or just
+            starting your journey of nurturing plants, we offer a wide variety
+            of options to suit every space and style. From vibrant indoor plants
+            that purify the air to low-maintenance succulents perfect for busy
             lifestyles, we bring nature closer to your home. Explore our curated
             selection of flowering plants, herbs, and decorative pots designed
-            to enhance your living space. Let's grow together and create a
+            to enhance your living space. Let&apos;s grow together and create a
             greener world, one plant at a time!
           </p>
           <button
